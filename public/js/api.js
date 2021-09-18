032e972383e5f533af49f024abab8e538dfa3452
TioChrisGt <87885964+TioChrisGt@users.noreply.github.com>
2021-09-18 17:46:23 -0600

    Add files via upload

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key="e59c91ea9c4d1207826e3f6d6b419891";
+let img_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
+
